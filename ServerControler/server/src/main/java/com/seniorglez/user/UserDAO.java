c511f496c9d81ca0e4d5055861b9bd191de420c6
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2021-03-04 20:42:19 +0100

    Fixed sql bug

--- ServerControler/server/src/main/java/com/seniorglez/user/UserDAO.java
+++ ServerControler/server/src/main/java/com/seniorglez/user/UserDAO.java
@@ -18,18 +18,17 @@ public class UserDAO {
     }
 
     public User getUserByUsername(String username) {
-        String url = "jdbc:mysql://db?" + "user=root&password=P@ssw0rd";
+        String url = "jdbc:mysql://db/myschema"; //Seriously, I don't know why I have to explicitly define the scheme 
         String query = "SELECT * FROM users WHERE username=?";
         ResultSet resultSet = null;
-        try (Connection connection = DriverManager.getConnection(url);
+        try (Connection connection = DriverManager.getConnection(url, "root", "P@ssw0rd");
             PreparedStatement statement = connection.prepareStatement(query)) {
             statement.setString(1, username);
-            resultSet = statement.executeQuery(query);
+            resultSet = statement.executeQuery();
             return (resultSet.next()) ? new User(resultSet.getString("username"),resultSet.getString("password")) : null;
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            
             if (resultSet!=null) try{ resultSet.close(); } catch(Exception e){e.printStackTrace();}; //beautiful line <3
         }
         return null;
