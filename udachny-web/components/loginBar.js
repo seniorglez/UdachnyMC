4997bc8286706eae6dfbd1018161d342e3f9badd
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2021-12-24 23:35:49 +0000

    Added client side rendering to the LoginBar

--- udachny-web/components/loginBar.js
+++ udachny-web/components/loginBar.js
@@ -1,13 +1,25 @@
 import Link from 'next/link'
+import { isLogged } from '../lib/user';
+import useSWR from 'swr'
 
 export function LoginBar({children}) {
+    
     return (
         <div className={'login-bar'}>
             <div className={'login-bar-container'}>
                 <div className={'login-bar-element'}>
-                    <Link href="/login">LOGIN</Link>
+                        <LoginLogout/>
                 </div>
             </div>
         </div>
     )
+}
+
+
+const fetcher = (...args) => isLogged();
+
+var LoginLogout = () => {
+    const { data, error } = useSWR('/api/user/123', fetcher)
+    if(error) return(<Link href="/login">LOGIN</Link>)  
+    return(<a>LOGOUT</a>)
 }
