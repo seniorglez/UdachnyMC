b35c0adf2a036efd970b6b369fc42feb42775712
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2021-12-25 00:38:20 +0000

    Improved logout navigation

--- udachny-web/components/loginBar.js
+++ udachny-web/components/loginBar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { isLogged, deleteToken } from '../lib/user';
-import Router from 'next/router';
+import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
 export function LoginBar({children}) {
@@ -19,14 +19,16 @@ export function LoginBar({children}) {
 
 const fetcher = (...args) => isLogged();
 
-const handleClick = (e) => {
-    e.preventDefault();
-    deleteToken();
-    Router.reload(window.location.pathname);
-}
-
 var LoginLogout = () => {
-    console.log('login,logout');
+
+    const router = useRouter()
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        deleteToken();
+        router.push('/')
+    }
+
     const { data, error } = useSWR('cookie', fetcher, { refreshInterval: 1000 }) //To show the correct text when login, maybe not the best solution
     if(data) return(<a onClick={(e) => handleClick(e)} >LOGOUT</a>)
     return(<Link href="/login">LOGIN</Link>); 
