6b876175b38052d5da258e3456363a445d9e5e2c
Edgar Lopez Herrera <edoardoelh@gmail.com>
2022-01-22 18:36:20 +0100

    wip

---
+++ ServerControler/server/src/main/java/com/seniorglez/aplication/endpoints/post/PostMinecraftLogs.java
@@ -0,0 +1,38 @@
+package com.seniorglez.aplication.endpoints.post;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+
+import com.seniorglez.aplication.login.ValidateToken;
+import com.seniorglez.domain.Reads;
+import com.seniorglez.domain.model.EndpointResponse;
+import com.seniorglez.infra.LogLineRequest;
+import com.seniorglez.infra.TokenManager;
+import com.google.gson.Gson;
+
+public class PostMinecraftLogs {
+
+    class JsonResponse {
+        private List<String> lines;
+
+        public JsonResponse(List<String> lines){
+            this.lines= lines;
+        }
+    }
+    
+    private Reads reads;
+
+    public PostMinecraftLogs(Reads reads){
+        this.reads =  reads;
+    }
+
+    public EndpointResponse execute(LogLineRequest logLineRequest){
+        if (new ValidateToken(new TokenManager()).execute(logLineRequest.getToken())) {
+            List<String> lines = this.reads.readLastLines(Path.of(new File("/minecraft-server/logs/latest.log").getPath()), 15);
+            Gson gson = new Gson();
+            return new EndpointResponse(200,"application/json",gson.toJson(lines));
+        }
+        return new EndpointResponse(403,"application/json","{\n\"result\":\"The token is not valid\"\n}");
+    }
+}
---
+++ ServerControler/server/src/main/java/com/seniorglez/infra/LogLineRequest.java
@@ -0,0 +1,14 @@
+package com.seniorglez.infra;
+
+public class LogLineRequest {
+    private int number;
+    private String token;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
